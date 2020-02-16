9d083e8ab7a17ecfe863da79d4cb2390aaa9bccd
zhengsongliang <295472781@qq.com>
2020-02-16 21:47:32 +0800

    init

---
+++ system1/src/test/java/com/test/slz/system1/System1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.slz.system1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class System1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
